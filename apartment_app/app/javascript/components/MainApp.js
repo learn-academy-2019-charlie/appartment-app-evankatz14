fe780106954a0c43499eed02e63afc4a2587c89d
Ubuntu <ubuntu@ip-172-31-25-177.us-west-2.compute.internal>
2019-07-31 05:43:22 +0000

    done for the night 7/30/2019

---
+++ apartment_app/app/javascript/components/MainApp.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class MainApp extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Hello World!</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MainApp
